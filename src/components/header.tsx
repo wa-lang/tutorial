31a44e1e0b2c406b9471ab9f0197a1bce2a67791
chai2010 <chaishushan@gmail.com>
2025-03-03 11:18:53 +0800

    统一用 Wa 名字

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,7 +6,7 @@ export function Header() {
   return (
     <header className="h-14 border-b flex items-center px-6 flex-shrink-0">
       <Logo className="w-6 h-6 mr-2" />
-      <h1 className="text-lg font-semibold mr-auto">WA Tutorial</h1>
+      <h1 className="text-lg font-semibold mr-auto">Wa Tutorial</h1>
       <LangToggle />
       <ModeToggle />
     </header>
